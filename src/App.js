62fc30512aba5b6e8dcdb120047a6d08fabb891a
andrvrb <andr-pnz@mail.ru>
2020-10-28 08:52:17 +0300

    props

--- src/App.js
+++ src/App.js
@@ -1,60 +1,17 @@
 import React from 'react'
-import {createGlobalStyle} from 'styled-components';
+
+import {GlobalStyle} from './Components/GlobalStyle'
 import {NavBar} from './Components/NavBar'
+import {Menu} from './Components/Menu'
 
-const GlobalStyle = createGlobalStyle`
-  html {
-    box-sizing: border-box;
-    }
-
-    *,
-    *::before,
-    *::after {
-      box-sizing:inherit;
-    }
-  
-    body {
-      margin: 0;
-      background-color: #f0f0f0;
-      font-family: Roboto, sans-serif;
-      font-size: 20px;
-      color: black;
-    }
-    
-img {
-  max-width: 100%;
-  height: auto;
-}
-
-    a {
-      text-decoration: none;
-      color: inherit;
-    }
-    ul {
-      list-style: none;
-      margin: 0;
-      padding: 0;
-    }
-  
-    h1,h2,h3 {
-      font-family: Pacifico;
-      padding: 0;
-      margin: 0;
-    }
-    p{
-      padding: 0;
-      margin: 0;
-    }
-`;
 
 function App() {
   return (
     <React.Fragment>
       <GlobalStyle/>
       <NavBar/>
-      <div className="App">
-        <h1>Hello React</h1>
-      </div>
+      <Menu/>
+
     </React.Fragment>
   );
 }
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import logoImg from '../image/logo.svg';
-import btnQuitImg from '../image/sign.svg';
+import btnLoginImg from '../image/sign.svg';
 
 const NavBarStyled = styled.header`
     position: absolute;
@@ -32,24 +32,11 @@ const ImgLogo = styled.img`
     width: 50px;
 `;
 
-const Button = styled.button`
-border: solid 0px #ce7910;
-color: #FFFFFF;
-padding: 9px 9px;
-background: linear-gradient(180deg, #ff8c00 5%, #ce7910 100%);
-box-shadow: 0px 10px 14px -7px #616174;
-cursor: pointer;
-user-select: none;
-display: inline-flex;
-justify-content: center;
-align-items: center;
-::hover {
-	background: linear-gradient(180deg, #ce7910 5%, #ff8c00 100%);
-}
-::active {
-	position: relative;
-	top: 1px;
-}
+const Login = styled.button`
+background-color: transparent;
+border-color: transparent;    
+color: white;
+font-size: 16px
 `
 
 export const NavBar = () => (
@@ -58,6 +45,6 @@ export const NavBar = () => (
             <ImgLogo src={logoImg} alt="logo"/>
             <H1>MrDonald's</H1>  
         </Logo>
-        <Button><img src={btnQuitImg} alt="quit"/></Button>  
+        <Login><img src={btnLoginImg} alt="quit"/><p>войти</p></Login>  
     </NavBarStyled>
 )
